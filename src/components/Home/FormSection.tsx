a68bd6d8df3d9fd041ee017627ebc02bb5944a1d
Mohsen Khademhosseini <mohsenpm77@gmail.com>
2025-05-24 11:49:28 +0330

    feat: get addressed - display insdie bottomsheet

--- src/components/Home/FormSection.tsx
+++ src/components/Home/FormSection.tsx
@@ -3,13 +3,10 @@
 import { useForm, FormProvider, FieldValues } from "react-hook-form";
 import { NumericInputs } from "../shared/NumericInputs";
 import { Button } from "../core/Button";
-
-import { useState } from "react";
-import { BottomSheet } from "../shared/BottomSheet";
+import { AddressField } from "./AddressField";
 
 export function FormSection() {
   const methods = useForm();
-  const [isOpen, setIsOpen] = useState(false);
 
   const submitHandler = (data: FieldValues) => {
     console.log(data);
@@ -22,33 +19,11 @@ export function FormSection() {
         onSubmit={methods.handleSubmit(submitHandler)}
       >
         <NumericInputs />
-        <div className="self-start pt-5">
-          <p className="text-md pb-2">آدرس جهت درج روی بیمه‌نامه</p>
-          <p className="text-sm">
-            لطفا آدرسی که می‌خواهید روی بیمه‌نامه درج شود، وارد کنید
-          </p>
-        </div>
-        <div className="w-full">
-          <Button
-            onClick={() => setIsOpen(true)}
-            className="py-2 rounded w-full"
-          >
-            انتخاب از آدرس‌های من
-          </Button>
-        </div>
+        <AddressField methods={methods} />
         <Button type="submit" color="black" className="self-end">
           تایید و ادامه
         </Button>
       </form>
-      <BottomSheet
-        headerTitle="انتخاب آدرس"
-        isOpen={isOpen}
-        onClose={() => setIsOpen(false)}
-      >
-        <div>
-          <p>First Sheet</p>
-        </div>
-      </BottomSheet>
     </FormProvider>
   );
 }
---
+++ src/components/core/RadioButton.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+import { useFormContext } from "react-hook-form";
+
+interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
+  value: string;
+  name: string;
+  required?: boolean;
+}
+
+export function RadioButton({
+  value,
+  name,
+  disabled,
+  required,
+
+  ...rest
+}: Props) {
+  const { register, watch, setValue } = useFormContext();
+  const isActive = watch(name) === value;
+
+  return (
+    <div>
+      <input
+        type="radio"
+        value={value}
+        disabled={disabled}
+        {...register(name, { required })}
+        onChange={() => setValue(name, value)}
+        {...rest}
+        className="hidden peer"
+      />
+      {isActive ? (
+        <Image width={16} height={16} src="radioOn.svg" alt="radio" />
+      ) : (
+        <Image src="radioOff.svg" width={16} height={16} alt="radio" />
+      )}
+    </div>
+  );
+}
