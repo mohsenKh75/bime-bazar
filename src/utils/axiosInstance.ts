4a3bad25536e0d3acd8bfe3ab4292c81385e3dca
Mohsen Khademhosseini <mohsenpm77@gmail.com>
2025-05-24 12:56:27 +0330

    feat: handle post data - include cookie by cross origin reqs

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: "https://front-end-task.bmbzr.ir/",
   timeout: 10000,
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
